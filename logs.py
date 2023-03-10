a15971cb0b3712e64df27e73a2f267728241a076
philip <philster36@hotmail.co.uk>
2023-03-10 15:40:14 +0000

    conversion page, conversion logging, hexadecimal in program

--- logs.py
+++ logs.py
@@ -13,7 +13,6 @@ Time: {now.strftime('%d/%m/%Y %H:%M:%S')}\n\
 {'-'*50}"
 
 
-
 def append_log(task: str, encryption_method: str, output: str):
     if not path.isfile(LOGS_FILENAME):
         file = open(LOGS_FILENAME, "w")
@@ -27,3 +26,17 @@ def append_log(task: str, encryption_method: str, output: str):
 
         file.write(format_log(task, encryption_method, output))
         file.close()    
+
+def append_custom_log(log: str):
+    if not path.isfile(LOGS_FILENAME):
+        file = open(LOGS_FILENAME, "w")
+        file.write(f"{'-'*50}\n{log}\n{'-'*50}")
+        file.close()
+        return
+    
+    with open ("logs.txt", "a") as file:
+        if os.path.getsize("logs.txt") <= 0:
+            file.write('-'*50)
+
+        file.write(f"\n{log}\n{'-'*50}")
+        file.close() 
--- menu/convert_page.py
+++ menu/convert_page.py
@@ -1,8 +1,10 @@
 from converter import ConversionMethod
 from converter.hexadecimal import HexadecimalConverter
 from menu import ask_user, ask_user_with_callback, textLogo, SEPARATOR_LENGTH, TerminalColors
+from logs import append_custom_log
 import re
 import os
+from datetime import datetime
 
 def decimal_check_callback(number: str):
     try:
@@ -24,6 +26,13 @@ def hex_check_callback(hexadecimal: str):
     
     return True
 
+def create_log(input: str, output:str, conversion_method: str):
+    now = datetime.now()
+    append_custom_log(f"Input: {input}\n\
+Output: {output}\n\
+Conversion: {conversion_method}\n\
+Time: {now.strftime('%d/%m/%Y %H:%M:%S')}")
+
 def manage_hexadecimal(convertion_method: HexadecimalConverter, settings: dict[str,str]):
     user_input = ask_user("", "Decimal > Hex", "Hex > Decimal")
     match user_input:
@@ -37,12 +46,15 @@ def manage_hexadecimal(convertion_method: HexadecimalConverter, settings: dict[s
     # Clears the terminal and shows us our result
     os.system('cls' if os.name == 'nt' else 'clear')
     print(textLogo)
+
+    # Our output
     print("-"*SEPARATOR_LENGTH)
     print(f"Before Conversion: {TerminalColors.BRIGHT_CYAN}{before_input}{TerminalColors.ENDC} \
 | After Conversion: {TerminalColors.BRIGHT_CYAN}{output}{TerminalColors.ENDC} \
 | {TerminalColors.BRIGHT_RED}Conversion: {user_input}{TerminalColors.ENDC}")
-
     print("-"*SEPARATOR_LENGTH)
+
+    create_log(before_input, output, user_input)
 
 def conversion_input(convertion_method: ConversionMethod, settings: dict[str,str]):
     match settings["method"]:
